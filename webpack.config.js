1ad3d6fe57dbf3de0441180d4253aee52987f973
Vesa Piittinen <vesap@verkkokauppa.com>
2017-05-11 14:18:39 +0300

    Update to Babel 6 and Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
-'use strict'
-
 var path = require('path')
 var webpack = require('webpack')
-var prod = process.env.NODE_ENV === 'production'
 
 var config = {
-    devtool: prod ? null : 'eval',
+    devtool: process.env.NODE_ENV === 'production' ? false : 'eval',
 
-    entry: [
-        path.join(__dirname, 'index.js')
-    ],
+    entry: ['./index.js'],
 
     output: {
         path: path.join(__dirname, 'dist'),
@@ -23,19 +18,21 @@ var config = {
     },
 
     module: {
-        loaders: [
-            { test: /\.js$/, loaders: ['babel?stage=0'], exclude: /node_modules/ }
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
         ]
     },
 
     plugins: [
-        new webpack.NoErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin()
     ]
 }
 
-if (prod) {
-    config.plugins.push(new webpack.optimize.DedupePlugin())
-    config.plugins.push(new webpack.optimize.OccurenceOrderPlugin(true))
+if (process.env.NODE_ENV === 'production') {
     config.plugins.push(new webpack.optimize.UglifyJsPlugin())
 }
 
